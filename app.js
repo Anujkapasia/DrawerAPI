c28d35f78264411005fbc6459585c86c28c54c3b
Anujkapasia <64798260+Anujkapasia@users.noreply.github.com>
2020-11-14 17:26:15 +0530

    awd

--- app.js
+++ app.js
@@ -10,10 +10,16 @@ const app = express();
 var fs = require('fs');
 
 app.use(express.static('pdf'))
+app.use(cors())
 
 app.post('/change', upload.single('photo'),(req,res)=>{
     fs.rename(`./uploads/${req.file.filename}`,`./uploads/${req.file.filename}.png`,()=>changeToPDf(req.file.filename))
-    res.json({link: `https://drawerapi.herokuapp.com:3030/pdf/${req.file.filename}.pdf`})
+    // res.json({link: `https://drawerapi.herokuapp.com:3030/pdf/${req.file.filename}.pdf`})
+    res.end(`https://drawerapi.herokuapp.com:3030/pdf/${req.file.filename}.pdf`);
+})
+
+app.get('/',(req,res)=>{
+    res.send("heyyyy")
 })
 
 async function changeToPDf (name){
